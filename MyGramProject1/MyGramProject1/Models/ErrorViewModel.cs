14c4dd1be499451159ecc5427880c65e57e768a3
Kris Melendez <kmelendez4235@southwestuniversity.edu>
2021-02-23 12:51:32 -0700

    Module 2 var

---
+++ MyGramProject1/MyGramProject1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyGramProject1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MyGramProject1/MyGramProject1/wwwroot/js/MyGramProj.js
@@ -0,0 +1,72 @@
+﻿var myDiv = document.getElementById("maindiv");
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "My Gram ";
+myDiv.appendChild(newSpan);
+
+var myDiv = document.getElementById("maindiv");
+var newSpan = document.createElement("button");
+newSpan.innerHTML = "Log In";
+myDiv.appendChild(newSpan);
+
+var myDiv = document.getElementById("maindiv");
+var newSpan = document.createElement("button");
+newSpan.innerHTML = "Sign Up";
+myDiv.appendChild(newSpan);
+
+var myDiv = document.getElementById("div2");
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "Image 1 ";
+myDiv.appendChild(newSpan);
+
+
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "Image 2 ";
+myDiv.appendChild(newSpan);
+
+
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "Image 3 ";
+myDiv.appendChild(newSpan);
+
+
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "Image 4 ";
+myDiv.appendChild(newSpan);
+
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "Image 5";
+myDiv.appendChild(newSpan);
+
+var myDiv = document.getElementById("div3");
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "About ";
+myDiv.appendChild(newSpan);
+
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "Blog ";
+myDiv.appendChild(newSpan);
+
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "Jobs ";
+myDiv.appendChild(newSpan);
+
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "Help";
+myDiv.appendChild(newSpan);
+
+var myDiv = document.getElementById("div4");
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "English ";
+myDiv.appendChild(newSpan);
+
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "2021 ";
+myDiv.appendChild(newSpan);
+
+var newSpan = document.createElement("span");
+newSpan.innerHTML = "MyGram";
+myDiv.appendChild(newSpan);
+
+var x = 5; //Number
+var y = "May the Force be with you"; //String
+var z = 'Second string line'; //String
